bf950c4f5085c9a192cbcd6afdd2c19cbf09c2e6
Koji Kobayashi <cozynooks@gmail.com>
2022-08-22 17:06:44 +0900

    chore: build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
-  "extends": [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/recommended',
-    'prettier/@typescript-eslint',
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/recommended",
+    "prettier/@typescript-eslint",
   ],
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "env": {
-    "node": true,
-    "es6": true
+  ignorePatterns: ["dist"],
+  plugins: ["@typescript-eslint"],
+  env: {
+    node: true,
+    es6: true,
   },
-  "parser": "vue-eslint-parser",
-  "parserOptions": {
-    "parser": "@typescript-eslint/parser"
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
   },
-  "rules": {
-    "semi": ["error", "always"],
-    "indent": ["error", 2],
-    "vue/html-indent": ["error", 2]
-  }
-}
+  rules: {
+    semi: ["error", "always"],
+    indent: ["error", 2],
+    "vue/html-indent": ["error", 2],
+  },
+};
